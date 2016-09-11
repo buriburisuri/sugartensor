6652b67c4315d587507dfcbc84d94d05f5ed6769
mansour <njkim@jamonglab.com>
2016-09-11 13:24:07 +0900

    initial commit

--- sugartensor/sg_data/mnist.py
+++ sugartensor/sg_data/mnist.py
@@ -52,28 +52,3 @@ class Mnist(object):
         self.test.num_batch = _test.labels.shape[0] // batch_size
 
 
-class Sample(object):
-
-    def __init__(self, batch_size=128):
-
-        # load data
-        import numpy as np
-        x = np.genfromtxt('/home/mansour/Downloads/sample.csv', delimiter=',', dtype=np.float32)
-        x = x[1:, 1:]
-
-        window = 384  # window size
-        max = 3000  # max value
-
-        # delete zero pad data
-        n = ((np.where(np.any(x, axis=1))[0][-1] + 1) // window) * window
-        x = x[:n] / max
-
-        # make to matrix
-        X = np.asarray([x[i:i+window] for i in range(n-window)])
-        np.random.shuffle(X)
-        X = np.expand_dims(X, axis=2)
-
-        # save to member variable
-        self.batch_size = batch_size
-        self.X = _data_to_tensor([X], batch_size, name='train')
-        self.num_batch = X.shape[0] // batch_size
